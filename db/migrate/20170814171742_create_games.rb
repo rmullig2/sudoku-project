e90f7432cbc8cb0784a47787aa0d4273a63d3d57
Ray Mulligan <rmullig2@optonline.net>
2017-08-14 16:36:49 -0400

    First user test written

--- db/migrate/20170814171742_create_games.rb
+++ db/migrate/20170814171742_create_games.rb
@@ -1,9 +1,9 @@
 class CreateGames < ActiveRecord::Migration[5.0]
   def change
     create_table :games do |t|
-      t.array :solution
-      t.array :start
-      t.array :current_state
+      t.integer :solution, array: true
+      t.integer :start, array: true
+      t.integer :current_state, array: true
       t.integer :score
       t.integer :moves
 
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "cannot create a user without a username" do
+    user = User.new
+    assert_not user.save
+  end
 end
