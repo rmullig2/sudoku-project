b0cedbbf16e294f59d80dd49fed5fbca2496ad0d
Ray Mulligan <rmullig2@optonline.net>
2017-08-17 13:42:39 -0400

    test that solution has correct length

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,25 +1,5 @@
-require 'matrix'
-require 'pry'
-
 class Solution < ApplicationRecord
   serialize :values
-  #attr_accessor :rows, :matrix
-  #
-  #def initialize(rows)
-  #  super
-  #  @rows = rows[:rows]
-  #  if @rows.length == 9
-  #    @matrix = Solution.createMatrix(@rows)
-  #  else
-  #    @matrix = nil
-  #    @rows = nil
-  #  end
-  #  #binding.pry
-  #end
-  #
-  #private
-  #  def self.createMatrix(rows)
-  #    Matrix[ rows[0], rows[1], rows[2], rows[3], rows[4], rows[5], rows[6], rows[7], rows[8] ]
-  #  end
+  validates :values, length: { is: 9 }
 end
 
