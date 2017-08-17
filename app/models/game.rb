5d0a9c3dc25e992b187a28743486dd194a73c616
Ray Mulligan <rmullig2@optonline.net>
2017-08-17 16:43:13 -0400

    create method to shuffle a solution

--- app/models/game.rb
+++ app/models/game.rb
@@ -4,10 +4,7 @@ require 'matrix'
 class Game < ApplicationRecord
   belongs_to :user
   validates :level, presence: true
-  attr_reader :start, :score, :moves, :solution
-  BLANK = ['', '', '', '', '', '', '', '', '']
-  #BLANK = []
-  FULL = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  attr_accessor :start, :score, :moves, :solution
   
   def initialize(user_id)
     super
@@ -19,30 +16,34 @@ class Game < ApplicationRecord
     @score = 0
     @moves = 0
     @solution = Matrix[*Solution.order("RANDOM()").first.values]
+    rand(10).times do
+      @solution.shuffle
+    end
   end
   
-  private
-    def self.shuffle
+    def shuffle
       rows, cols = [], []
       for i in 0..8
         rows[i] = @solution.row(i)
-        cols(i) = @solution.transpose.row(i)
+        cols[i] = @solution.transpose.row(i)
       end
       
-      case rand(4)
+      case rand(5)
       when 0    # swap single rows
         single_swap(rows)
-        @solution = Matrix[rows]
+        @solution = Matrix[*rows]
       when 1    # swap single columns
         single_swap(cols)
-        @solution = Matrix[cols]
+        @solution = Matrix[*cols]
         @solution = @solution.transpose
       when 2    # swap row groups
         group_swap(rows)
-        @solution = Matrix[rows]
+        @solution = Matrix[*rows]
       when 3    # swap column groups
         group_swap(cols)
-        @solution = Matrix[cols]
+        @solution = Matrix[*cols]
+        @solution = @solution.transpose
+      when 4    # transpose rows and columns
         @solution = @solution.transpose
       end
     end
@@ -50,8 +51,8 @@ class Game < ApplicationRecord
     def single_swap(vects)
 #     Setup variables to be used in the shuffle
       multiplier, excluded = rand(3)*3, rand(3)
-      idx = [0,1,2].select do
-        |elem| != excluded
+      idx = [0,1,2].select do |elem|
+        elem != excluded
       end
       vects[idx[0]+multiplier],vects[idx[1]+multiplier] = vects[idx[1]+multiplier],vects[idx[0]+multiplier]
     end
@@ -59,8 +60,8 @@ class Game < ApplicationRecord
     def group_swap(vects)
 #     Setup variables to be used in the shuffle
       excluded = rand(3)
-      grps = [0,1,2].select do
-        |elem| != excluded
+      grps = [0,1,2].select do |elem|
+        elem != excluded
       end
       vects[grps[0]*3],vects[grps[1]*3] = vects[grps[1]*3],vects[grps[0]*3]
       vects[grps[0]*3+1],vects[grps[1]*3+1] = vects[grps[1]*3+1],vects[grps[0]*3+1]
