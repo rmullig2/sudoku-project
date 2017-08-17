39a2a41b2440daca11f8551e4525ddbfe7659495
Ray Mulligan <rmullig2@optonline.net>
2017-08-16 20:15:21 -0400

    refactor solution model

--- app/models/solution.rb
+++ app/models/solution.rb
@@ -1,3 +1,24 @@
+require 'matrix'
+require 'pry'
+
 class Solution < ApplicationRecord
-  validates :rows, length: { is: 9 }
+  attr_accessor :rows, :matrix
+  
+  def initialize(rows)
+    super
+    @rows = rows[:rows]
+    if @rows.length == 9
+      @matrix = Solution.createMatrix(@rows)
+    else
+      @matrix = nil
+      @rows = nil
+    end
+    #binding.pry
+  end
+  
+  private
+    def self.createMatrix(rows)
+      Matrix[ rows[0], rows[1], rows[2], rows[3], rows[4], rows[5], rows[6], rows[7], rows[8] ]
+    end
 end
+
--- test/models/solution_test.rb
+++ test/models/solution_test.rb
@@ -1,9 +1,10 @@
 require 'test_helper'
+require 'pry'
 
 class SolutionTest < ActiveSupport::TestCase
   test "solution needs to have a length of nine" do
     testValues = [ [1], [2], [3], [4], [5], [6], [7], [8] ]
-    sol_test = Solution.new(rows: testValues)
-    assert_not sol_test.save
+    sol_test = Solution.create(rows: testValues)
+    assert_nil(sol_test.rows)
   end
 end
