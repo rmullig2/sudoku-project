8e630a24f7765b3737732c0c9411d8f7cbdb2a6a
Ray Mulligan <rmullig2@optonline.net>
2017-08-19 20:41:30 -0400

    created user controller actions

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,8 +5,12 @@ class Api::V1::UsersController < ApplicationController
   end
   
   def create
+    @user = User.create(name: params[:name])
+    render @user
   end
   
   def show
+    @user = User.find_by_slug(params[:name])
+    render @user
   end
 end
