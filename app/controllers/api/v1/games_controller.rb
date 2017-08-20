00a36090066c195838f30370b00be401e9f66927
Ray Mulligan <rmullig2@optonline.net>
2017-08-19 20:59:18 -0400

    create game controller methods

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,14 +1,27 @@
 class Api::V1::GamesController < ApplicationController
   def index
+    user = User.find_by_slug(params[:name])
+    games = user.games
+    render json: games
   end
   
   def create
+    user = User.find_by_slug(params[:name])
+    game = user.create(level: params[:level])
+    render json: game
   end
   
   def show
+    user = User.find_by_slug(params[:name])
+    game = user.games.find(params[:id])
+    render json: game
   end
   
   def update
+    user = User.find_by_slug(params[:name])
+    game = user.games.find(params[:id])
+    game.current = params[:current_state]
+    game.save
   end
   
   def high
