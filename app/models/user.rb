bc5419eb894f6b168e52e69fc52f2dc6e6cd19dc
Ray Mulligan <rmullig2@optonline.net>
2017-08-27 18:15:36 -0400

    add password to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,9 @@ class User < ApplicationRecord
   before_create :create_slug
   validates :name, presence: true, uniqueness: true
   has_many :games
+  has_secure_password
   
   def self.find_by_slug(slug)
     self.find_by(name: slug)
   end
 end
-
-#User.find_by_slug("derekj")
--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,9 @@ ActiveRecord::Schema.define(version: 20170816202152) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
