6f4a0137c6dc870cf56b064948b5806aa835a517
Ray Mulligan <rmullig2@optonline.net>
2017-08-29 14:13:07 -0400

    adjust fixtures for bcrypt

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -1,24 +1,24 @@
 class Api::V1::GamesController < ApplicationController
   def index
-    user = User.find_by_slug(params[:name])
+    user = User.find_by(name: params[:name])
     games = user.games
     render json: games
   end
   
   def create
-    user = User.find_by_slug(params[:name])
+    user = User.find_by(name: params[:name])
     game = user.games.create({level: params[:level]})
     render json: game
   end
   
   def show
-    user = User.find_by_slug(params[:name])
+    user = User.find_by(name: params[:name])
     game = user.games.find(params[:id])
     render json: game
   end
   
   def update
-    user = User.find_by_slug(params[:name])
+    user = User.find_by(name: params[:name])
     game = user.games.find(params[:id])
     game.current = params[:current_state]
     game.save
--- test/integration/routes_test.rb
+++ test/integration/routes_test.rb
@@ -9,17 +9,17 @@ class RoutesTest < ActionDispatch::IntegrationTest
     assert_routing({ path: 'api/v1/users', method: :post }, { controller: 'api/v1/users', action: 'create', format: :json })
   end
     test "users_show_test" do
-    assert_routing({ path: 'api/v1/users/:slug', method: :get }, { format: :json, controller: 'api/v1/users', action: 'show', slug: ":slug" })
+    assert_routing({ path: 'api/v1/users/:name', method: :get }, { format: :json, controller: 'api/v1/users', action: 'show', name: ":name" })
   end
 
   test "games_index_test" do
-    assert_routing({ path: 'api/v1/users/:user_slug/games', method: :get }, { format: :json, controller: 'api/v1/games', action: 'index', user_slug: ":user_slug" })
+    assert_routing({ path: 'api/v1/users/:user_name/games', method: :get }, { format: :json, controller: 'api/v1/games', action: 'index', user_name: ":user_name" })
   end
   test "games_create_test" do
-    assert_routing({ path: 'api/v1/users/:user_slug/games', method: :post }, { format: :json, controller: 'api/v1/games', action: 'create', user_slug: ":user_slug" })
+    assert_routing({ path: 'api/v1/users/:user_name/games', method: :post }, { format: :json, controller: 'api/v1/games', action: 'create', user_name: ":user_name" })
   end
   test "games_show_test" do
-    assert_routing({ path: 'api/v1/users/:user_slug/games/:id', method: :get }, { format: :json, controller: 'api/v1/games', action: 'show', user_slug: ":user_slug", id: ":id" })
+    assert_routing({ path: 'api/v1/users/:user_name/games/:id', method: :get }, { format: :json, controller: 'api/v1/games', action: 'show', user_name: ":user_name", id: ":id" })
   end
   test "high_scores" do
     assert_routing({ path: 'api/v1/high_scores', method: :get }, { format: :json, controller: 'api/v1/games', action: 'high' })
