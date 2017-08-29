1a90f6973f407bdadda33c49fd5f7b306cc59884
Ray Mulligan <rmullig2@optonline.net>
2017-08-29 14:41:13 -0400

    build out index.js

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {Provider} from 'redux';
+import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/index';
+import { Router, Route, browserHistory } from 'react-router'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+    <Provider store={store} >
+      <Router history={browserHistory}>
+        <Route path="/" component={App} />
+      </Router>
+    </Provider>, document.getElementById('root')
+);
+
+//ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
---
+++ client/src/reducers/index.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+//import catsReducer from './cats_reducer'
+
+var rootReducer = combineReducers({
+  //cats: catsReducer,
+});
+
+export default rootReducer
