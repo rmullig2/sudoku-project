4ecf9de71bb88bfad5557e096307ffb9a3b9b3f2
Ray Mulligan <rmullig2@optonline.net>
2017-09-04 21:06:06 -0400

    add form reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authReducer from './auth_reducer'
+import { reducer as FormReducer } from 'redux-form';
 
 var rootReducer = combineReducers({
   auth: authReducer,
+  form: FormReducer
 });
 
 export default rootReducer
