5fbd9595c13e7e427481cd26b499328a92405f6b
Ray Mulligan <rmullig2@optonline.net>
2017-09-04 22:09:08 -0400

    build login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,41 @@
+//import React from 'react';
+//
+//const Login = (props) => {
+//    return <div>Login</div>;
+//}
+//
+//export default Login;
+
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
 const Login = (props) => {
-    return <div>Login</div>;
+  const handleFormSubmit = (values) => {
+    console.log(values);
+  };
+
+  return(
+    <div className="container">
+      <div className="col-md-6 col-md-offset-3">
+        <h2 className="text-center">Log In</h2>
+        <form onSubmit={this.handleFormSubmit}>
+          <fieldset className="form-group">
+            <label>Email</label>
+            <Field name="email" component="input" className="form-control" type="text" placeholder="Email"/>
+          </fieldset>
+    
+          <fieldset className="form-group">
+            <label>Password</label>
+            <Field name="password" component="input" className="form-control" type="password" placeholder="Password"/>
+          </fieldset>
+    
+          <button action="submit" className="btn btn-primary">Sign In</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
-export default Login;
+export default reduxForm({
+  form: 'login'
+})(Login);
