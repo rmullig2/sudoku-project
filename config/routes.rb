1a37787256da66d25b761508171fd1dbae6e3251
Ray Mulligan <rmullig2@optonline.net>
2017-10-21 17:05:48 -0400

    update route table

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1, defaults: { format: :json } do
-      resources :users, param: :name, only: [ :index, :create, :show ] do
-        resources :games, only: [:index, :create, :show, :update]
-      end
-      get 'high_scores', to: 'games#high'
+      #resources :users, param: :name, only: [ :index, :create, :show ] do
+      #  resources :games, only: [:index, :create, :show, :update]
+      #end
+      get 'game', to: 'games#create'
+      get 'high_scores', to: 'scores#index'
+      post 'high_scores', to: 'scores#create'
     end
   end
 end
