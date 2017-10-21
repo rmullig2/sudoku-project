72c8aba93e52dcf4f838b70a9257cbaac3536a66
Ray Mulligan <rmullig2@optonline.net>
2017-10-21 13:17:48 -0400

    Recode the game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,41 +2,40 @@ require 'pry'
 require 'matrix'
 
 class Game < ApplicationRecord
-  belongs_to :user
-  validates :level, presence: true
-  attr_accessor :start, :score, :moves, :solution
+attr_accessor :start, :solution
   
-  def initialize(params)
+  def initialize()
     super
-    #user = User.find(user_id)
-    @level = params[:level]
-    @start = [ ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''],
-               ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''],
-               ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', ''], ['', '', '', '', '', '', '', '', '']]
-    @current = []
-    @score = 0
-    @moves = 0
-    @solution = Matrix[*Solution.order("RANDOM()").first.values]
-    rand(10).times do
+    # Define the starting board and the solution
+    @start = Array.new(9){Array.new(9)}
+    @solution = [ [4, 7, 3, 5, 2, 8, 1, 9, 6], [2, 5, 1, 6, 7, 9, 8, 4, 3], [6, 8, 9, 1, 4, 3, 2, 7, 5],
+                  [1, 4, 7, 9, 3, 2, 5, 6, 8], [3, 6, 5, 8, 1, 7, 4, 2, 9], [9, 2, 8, 4, 5, 6, 7, 3, 1],
+                  [8, 3, 4, 7, 9, 1, 6, 5, 2], [7, 9, 6, 2, 8, 5, 3, 1, 4], [5, 1, 2, 3, 6, 4, 9, 8, 7] ]
+    1000.times do
       shuffle
     end
     @start = create_start(@start)
+    @start = @start.flatten()
+    @solution = @solution.flatten()
   end
   
     def shuffle
       rows, cols = [], []
       for i in 0..8
-        rows[i] = @solution.row(i)
-        cols[i] = @solution.transpose.row(i)
+        #rows[i] = @solution.row(i)
+        #cols[i] = @solution.transpose.row(i)
+        rows[i] = @solution[i]
+        cols[i] = @solution.transpose[i]
       end
       
-      case rand(5)
+      case rand(1)
       when 0    # swap single rows
         single_swap(rows)
-        @solution = Matrix[*rows]
+        @solution = rows
       when 1    # swap single columns
         single_swap(cols)
-        @solution = Matrix[*cols]
+        #@solution = Matrix[*cols]
+        @solution = cols
         @solution = @solution.transpose
       when 2    # swap row groups
         group_swap(rows)
@@ -72,7 +71,7 @@ class Game < ApplicationRecord
     
     def create_start(arr)
       reveal = []
-      num_shown = 37 - @level * 3
+      num_shown = 29
       while reveal.length < num_shown
         n = rand(81)
         if !reveal.include? n
@@ -80,7 +79,7 @@ class Game < ApplicationRecord
         end
       end
       for i in reveal
-        arr[i/9][i%9] = @solution.element(i/9,i%9)
+        arr[i/9][i%9] = @solution[i/9][i%9]
       end
       arr
     end
