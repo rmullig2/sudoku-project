d02d3761c6ed17b8e1bf9c2e1bf92dc6e1d4a54f
Ray Mulligan <rmullig2@optonline.net>
2017-10-21 14:46:43 -0400

    Create migration for scores

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171021161153) do
+ActiveRecord::Schema.define(version: 20171021183552) do
 
   create_table "games", force: :cascade do |t|
     t.integer "start"
     t.integer "soution"
+  end
+
+  create_table "scores", force: :cascade do |t|
+    t.string  "name"
+    t.integer "time"
   end
 
 end
