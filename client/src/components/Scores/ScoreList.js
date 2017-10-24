b8978c97046e9949a5ab41bf711e608e0c6d6306
Ray Mulligan <rmullig2@optonline.net>
2017-10-24 19:27:25 -0400

    Create score list component

---
+++ client/src/components/Scores/ScoreList.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Score from './Score.js'
+
+const ScoreList = ( {scores} ) =>
+  <ol>
+    {scores.forEach(
+      (score) =>
+        <li key={score.id.toString()}>
+          <Score {score} />
+        </li>
+    )}
+  </ol>
+  
+export default ScoreList
