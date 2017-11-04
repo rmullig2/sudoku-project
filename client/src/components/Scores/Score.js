4ebde2b2a195b3c1921b5668a949c6c45bd034b4
Ray Mulligan <rmullig2@optonline.net>
2017-11-04 14:20:18 -0400

    Format scores to minutes and seconds

--- client/src/components/Scores/Score.js
+++ client/src/components/Scores/Score.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
 const Score = ({ name, time }) =>
-  <span>{name}  {time}</span>
+  <span>{name} - {Math.floor(time/60000)} minutes, {Math.round(time/60000%60)} seconds</span>
 
 export default Score
