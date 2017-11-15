ecaaad934dcc79e33b879899a56622cfcdeb1471
Ray Mulligan <rmullig2@optonline.net>
2017-11-15 18:43:09 -0500

    change a href to Link in Home

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import logo from '../sudoku.png';
 import './home.css';
+import { Link } from 'react-router';
 
 const Home = () => {
   return(
     <div>
       <p id="press">Click Play to get started or about to learn more about Sudoku.</p>
       <div id="link">
-        <a href="/about">About </a>
-        <a href="/game">Play </a>
-        <a href="/scores">High Scores</a>
+        <Link to="/about">About</Link>
+        <Link to="/game">Play</Link>
+        <Link to="/scores">High Scores</Link>
       </div>
     </div>
   )
