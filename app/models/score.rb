12019512aec6371d7313101e73063311e310831f
Ray Mulligan <rmullig2@optonline.net>
2017-11-14 19:13:50 -0500

    modify rails test

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,5 +1,5 @@
 class Score < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: true, length: { maximum: 3 }
   validates :time, presence: true
   #attr_accessor :name, :time
 end
