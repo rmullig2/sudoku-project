eaa6a16c453cf05ceda55f14943bf4f5907b337b
Ray Mulligan <rmullig2@optonline.net>
2017-11-20 01:45:22 -0500

    refactor Score component

--- client/src/components/Scores/Score.js
+++ client/src/components/Scores/Score.js
@@ -7,22 +7,19 @@ class Score extends Component {
     super();
     this.state = { likes: 0 };
   }
+  
   increment = () => {
-    var inc = this.state.likes + 1;
     this.setState({
-      likes: inc,
+      likes: this.state.likes + 1,
     })
   }
   
   render(){
-    console.log(this);
+    //console.log(this);
     return (<span>{this.props.name} - {Math.floor(this.props.time/60000)} minutes,
     {Math.round(this.props.time/60000%60)} seconds  {this.state.likes}
     <input type="button" value="Like" onClick={ this.increment } /></span>)
   }
 }
-
-//const Score = ({ name, time }) =>
-//  <span>{name} - {Math.floor(time/60000)} minutes, {Math.round(time/60000%60)} seconds</span>
 
 export default Score
