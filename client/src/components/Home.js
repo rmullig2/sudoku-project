bb532ae33bc642a2a9bbd8d6e095df4b39a4ce92
Ray Mulligan <rmullig2@optonline.net>
2017-11-27 14:10:08 -0500

    Cleanup

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from '../sudoku.png';
 import './home.css';
 import { Link } from 'react-router';
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-//import getScore from './actions.js';
 import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers/index';
 import thunk from 'redux-thunk';
